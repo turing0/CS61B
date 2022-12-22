303620b2ba1cf609402d029500f8a1812f492cca
turing0 <turing175@gmail.com>
2022-12-22 22:12:25 +0800

    update proj0

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -18,15 +18,15 @@ public class TestArrayDequeEC {
         int N = 25;
         String msg = "";
         for (int i = 0; i < N; i += 1) {
-            int operationNumber = StdRandom.uniform(0, 2);
-            if (operationNumber == 0) {
+            double  operationNumber = StdRandom.uniform();
+            if (operationNumber < 0.5) {
                 // addLast
                 int randVal = StdRandom.uniform(0, 100);
                 bad.addLast(randVal);
                 good.addLast(randVal);
                 msg = msg + "addLast("+randVal+")\n";
 //                System.out.printf("addLast(%d)\n", randVal);
-            } else if (operationNumber == 1) {
+            } else {
                 // addFirst
                 int randVal = StdRandom.uniform(0, 100);
                 bad.addFirst(randVal);
@@ -41,28 +41,23 @@ public class TestArrayDequeEC {
         assertEquals(msg, blsize, size);
 
         for (int i = 1; i < N; i += 1) {
-            int operationNumber = StdRandom.uniform(0, 2);
-            if (operationNumber == 0) {
+            double operationNumber = StdRandom.uniform();
+            if (operationNumber < 0.5) {
                 // removeFirst
-                if (bad.size()>0 && good.size()>0) {
-                    Integer v1=-1, v2=-1;
-                    v1 = bad.removeFirst();
-                    msg = msg + "removeFirst(): "+v1+"\n";
-//                    System.out.printf("removeFirst(): %d\n", v1);
-                    v2 = good.removeFirst();
-                    assertEquals(msg, v2, v1);
-                }
+                Integer v1=-1, v2=-1;
+                v1 = bad.removeFirst();
+                msg = msg + "removeFirst(): "+v1+"\n";
+                v2 = good.removeFirst();
+                assertEquals(msg, v2, v1);
             }
-            else if (operationNumber == 1) {
+            else {
                 // removeLast
-                if (bad.size()>0 && good.size()>0) {
-                    Integer v1=-1, v2=-1;
-                    v1 = bad.removeLast();
-                    msg = msg + "removeLast(): "+v1+"\n";
-//                    System.out.printf("removeLast(): %d\n", v1);
-                    v2 = good.removeLast();
-                    assertEquals(msg, v2, v1);
-                }
+                Integer v1=-1, v2=-1;
+                v1 = bad.removeLast();
+                msg = msg + "removeLast(): "+v1+"\n";
+                v2 = good.removeLast();
+                assertEquals(msg, v2, v1);
+
             }
         }
 
