b719a60b03e4b69346499f77c4b2b31b6b2500c8
turing0 <turing175@gmail.com>
2022-12-22 21:29:02 +0800

    update proj1ec

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -15,7 +15,7 @@ public class TestArrayDequeEC {
         StudentArrayDeque<Integer> bad = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> good = new ArrayDequeSolution<>();
 
-        int N = 500;
+        int N = 1000;
         String msg = "";
         for (int i = 0; i < N; i += 1) {
             int operationNumber = StdRandom.uniform(0, 5);
@@ -25,14 +25,12 @@ public class TestArrayDequeEC {
                 bad.addLast(randVal);
                 good.addLast(randVal);
                 msg = msg + "addLast("+randVal+")\n";
-//                System.out.printf("addLast(%d)\n", randVal);
             } else if (operationNumber == 1) {
                 // addFirst
                 int randVal = StdRandom.uniform(0, 100);
                 bad.addFirst(randVal);
                 good.addFirst(randVal);
                 msg = msg + "addFirst("+randVal+")\n";
-//                System.out.printf("addFirst(%d)\n", randVal);
             } else if (operationNumber == 2) {
                 // size
                 int size = bad.size();
@@ -40,33 +38,23 @@ public class TestArrayDequeEC {
                 msg = msg + "size(): "+size+"\n";
                 assertEquals(msg, blsize, size);
 //                System.out.println("size: " + size);
-            }
-            else if (operationNumber == 3) {
+            } else if (operationNumber == 3) {
                 // removeFirst
                 if (bad.size()>0 && good.size()>0) {
-                    Integer v1=-1, v2=-1;
-                    v1 = bad.removeFirst();
-                    msg = msg + "removeFirst(): "+v1+"\n";
-//                    System.out.printf("removeFirst(): %d\n", v1);
-                    v2 = good.removeFirst();
-                    assertEquals(msg, v2, v1);
+//                    msg = msg + "removeFirst(): "+v1+"\n";
+                    msg = msg + "removeFirst()\n";
+                    assertEquals(msg, good.removeFirst(), bad.removeFirst());
                 }
-            }
-            else if (operationNumber == 4) {
+            } else if (operationNumber == 4) {
                 // removeLast
                 if (bad.size()>0 && good.size()>0) {
-                    Integer v1=-1, v2=-1;
-                    v1 = bad.removeLast();
-                    msg = msg + "removeLast(): "+v1+"\n";
-//                    System.out.printf("removeLast(): %d\n", v1);
-                    v2 = good.removeLast();
-                    assertEquals(msg, v2, v1);
+//                    msg = msg + "removeLast(): "+v1+"\n";
+                    msg = msg + "removeLast()\n";
+                    assertEquals(msg, good.removeLast(), bad.removeLast());
                 }
             }
         }
 
-
     }
-
 
 }
