c5d30f7826bbcddf3779ccce4d52eb3ece871fd9
turing0 <turing175@gmail.com>
2022-12-22 17:25:58 +0800

    update for lab4a

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -4,11 +4,21 @@
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
+//    public static int nextNumber(int n) {
+//        if (n % 2 == 0) {
+//            return n / 2;
+//        } else {
+//            return 3 * n + 1;
+//        }
+//    }
+    // for Lab 4A
     public static int nextNumber(int n) {
-        if (n % 2 == 0) {
-            return n / 2;
-        } else {
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
             return 3 * n + 1;
+        } else {
+            return n * 2;
         }
     }
 
